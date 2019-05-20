fb449b94c109da00aa84628dda1aed684d12136a
Soroush Khosravi <sorush.kh68@gmail.com>
2019-05-20 17:05:10 +0100

    If the number is devisible by 5, buzz comes out.

--- jasmine-standalone-3.4.0/spec/FizzbuzzSpec.js
+++ jasmine-standalone-3.4.0/spec/FizzbuzzSpec.js
@@ -1,6 +1,12 @@
 describe("Fizzbuz",function(){
+  beforeEach(function(){
+    game = new Game;
+  })
   it("If the number is devisible by 3, the output is fizz",function(){
-    game = new Game()
     expect(game.fizzBuzz(3)).toEqual("fizz")
+  })
+
+  it("If the number is devisible by 5, the output is buzz",function(){
+    expect(game.fizzBuzz(5)).toEqual("buzz")
   })
 })
--- jasmine-standalone-3.4.0/src/Fizzbuzz.js
+++ jasmine-standalone-3.4.0/src/Fizzbuzz.js
@@ -4,6 +4,8 @@ function Game(){
 Game.prototype.fizzBuzz = function(number){
   if(number%3 === 0){
     return("fizz")
+  } else if (number%5 === 0){
+    return("buzz")
   }
 
 }
