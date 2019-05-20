6437187af6c06c9fd931266342022bdd9748e9cc
Soroush Khosravi <sorush.kh68@gmail.com>
2019-05-20 17:11:57 +0100

    If the number is devisible by 15, fizzbuzz comes out of the method.

--- jasmine-standalone-3.4.0/spec/FizzbuzzSpec.js
+++ jasmine-standalone-3.4.0/spec/FizzbuzzSpec.js
@@ -9,4 +9,8 @@ describe("Fizzbuz",function(){
   it("If the number is devisible by 5, the output is buzz",function(){
     expect(game.fizzBuzz(5)).toEqual("buzz")
   })
+
+  it("if the number is devisible by 15, the output is fizzbuzz",function(){
+    expect(game.fizzBuzz(15)).toEqual("fizzbuzz")
+  })
 })
--- jasmine-standalone-3.4.0/src/Fizzbuzz.js
+++ jasmine-standalone-3.4.0/src/Fizzbuzz.js
@@ -2,10 +2,12 @@ function Game(){
 
 }
 Game.prototype.fizzBuzz = function(number){
-  if(number%3 === 0){
-    return("fizz")
+  if(number%15 === 0){
+    return("fizzbuzz")
   } else if (number%5 === 0){
     return("buzz")
+  } else if(number%3 === 0){
+    return("fizz")
   }
 
 }
